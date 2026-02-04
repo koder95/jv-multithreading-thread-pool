575615200dd94a921d763f4a153353352e17722f
Kamil Jan Mularski <kammul@outlook.com>
2026-02-04 20:54:29 +0100

    My implementation of MyThread

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,8 +1,11 @@
 package core.basesyntax;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.IntStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,7 +13,28 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        List<Future<String>> futures;
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        try {
+            futures = executor.invokeAll(
+                    IntStream.range(0, 20)
+                            .mapToObj(i -> new MyThread())
+                            .toList()
+            );
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+        futures.forEach(future -> {
+            try {
+                logger.info(future.get());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        executor.shutdown();
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,22 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private final long duration;
+
+    public MyThread() {
+        this.duration = Math.round(1 + Math.random() * 4) * 100;
+    }
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Thread.sleep(duration);
+        return String.format("Task duration was %d ms, execution finished at %s",
+                duration,
+                LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSS"))
+        );
     }
 }
